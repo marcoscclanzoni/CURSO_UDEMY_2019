6d2e3bf5bd89eae466736f708b99725d0d6b9aa8
Marcos Lanzoni <marcoslanzoni@gmail.com>
2019-08-17 18:12:31 -0300

    alterado Texto Alo Mundo

--- AloMundo/Program.cs
+++ AloMundo/Program.cs
@@ -6,7 +6,7 @@ namespace AloMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Alo Mundo");
         }
     }
 }
