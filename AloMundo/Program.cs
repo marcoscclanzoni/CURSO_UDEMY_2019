c504bcf4314f6d2d2d35069a57d93f5a0b204a36
Marcos Lanzoni <marcoslanzoni@gmail.com>
2019-08-17 17:41:16 -0300

    iniciando primeito comit

---
+++ AloMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AloMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
