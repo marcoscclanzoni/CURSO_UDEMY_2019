eade0a3941d18d6239aa79c8abd0afe60b8cca46
Marcos Lanzoni <marcoslanzoni@gmail.com>
2019-08-24 22:09:34 -0300

    if else

---
+++ Aula_Calculos/Aula_Calculos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula_Calculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+           
+            
+        {
+            int x = 10;
+            Console.WriteLine("Bom dia ");
+            if (x < 5)
+            {
+                Console.WriteLine("Boa Tarde");
+            }
+            Console.WriteLine("Boa Noite");
+        }
+    }
+}
---
+++ if_else/if_else/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+// esta logica testa se o numero é par ou impar
+// data 24/08/2019
+// autor marcos lanzoni
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com um numero inteiro ........"); // faz a pergunta para entrar com numero inteiro
+            int x = int.Parse(Console.ReadLine()); // grava o numero na variavel "x" 
+            if (x % 2 == 0) // aqui verifica se o resto da divisão é igual a zero comprovando que é par
+            {
+                Console.WriteLine("O numero digitado é par "); // aqui informa que é  par
+            }
+            else {
+                Console.WriteLine("O numero digitado é impar"); // caso o resto da divisão não seja zero é impar
+
+            }
+
+        }
+    }
+}
