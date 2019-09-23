5f2903cad34f2582472a4a42aac946422a2a9ead
Marcos Lanzoni <33994819+marcoscclanzoni@users.noreply.github.com>
2019-09-22 22:33:21 -0300

    Update Cod

--- AloMundo/Program.cs
+++ AloMundo/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace AloMundo
 {
@@ -7,6 +7,10 @@ namespace AloMundo
         static void Main(string[] args)
         {
             Console.WriteLine("Alo Mundo");
+						/*
+						inserido comentarios de multiplas linhas 
+						*/
+
         }
     }
 }
